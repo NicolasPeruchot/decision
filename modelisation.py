0514cb7434a5b6dd80b8406b25f2dc95900c434c
Guilhem Prince <guilhem.prince@student-cs.fr>
2023-01-17 14:45:49 +0100

    add s to use addConstrs method

--- modelisation.py
+++ modelisation.py
@@ -37,9 +37,7 @@ def init_model():
 
 
 def create_decision_variables(model, n_staff, horizon, n_qualification, n_jobs):
-    X = model.addMVar(
-        (n_staff, horizon, n_qualification, n_jobs), vtype=GRB.BINARY, name="X"
-    )
+    X = model.addMVar((n_staff, horizon, n_qualification, n_jobs), vtype=GRB.BINARY, name="X")
     J = model.addMVar(n_jobs, vtype=GRB.BINARY, name="J")
     D = model.addMVar((n_jobs, 3), lb=0, ub=horizon + 1, vtype=GRB.INTEGER, name="D")
     model.update()
@@ -52,8 +50,7 @@ def add_constraints_for_J(model, X, J, jobs, qualifications=data["qualifications
             J[job],
             True,
             (
-                sum(X[:, :, index_k, job])
-                >= jobs[job]["working_days_per_qualification"][k]
+                sum(X[:, :, index_k, job]) >= jobs[job]["working_days_per_qualification"][k]
                 for index_k, k in enumerate(qualifications)
             ),
         )
@@ -61,8 +58,7 @@ def add_constraints_for_J(model, X, J, jobs, qualifications=data["qualifications
             J[job],
             False,
             not (
-                sum(X[:, :, index_k, job])
-                >= jobs[job]["working_days_per_qualification"][k]
+                sum(X[:, :, index_k, job]) >= jobs[job]["working_days_per_qualification"][k]
                 for index_k, k in enumerate(qualifications)
             ),
         )
@@ -84,39 +80,35 @@ def add_constraints_for_D(model, X, J, D, horizon):
 
 
 def in_qualification(i, k):
-    data = {
-        data["staff"][i]["name"]: data["staff"][i] for i in range(len(data["staff"]))
-    }
+    data = {data["staff"][i]["name"]: data["staff"][i] for i in range(len(data["staff"]))}
     data = data[i]["qualifications"]
     return k in data
 
 
 def qualification_constraint(model, n_staff, horizon, n_qualifs, n_jobs, X):
-    model.addConstr(
+    model.addConstrs(
         X[i, j, k, l] == 0
-        for i in n_staff
-        for j in horizon
-        for k in n_qualifs
-        for l in n_jobs
+        for i in range(n_staff)
+        for j in range(horizon)
+        for k in range(n_qualifs)
+        for l in range(n_jobs)
         if not in_qualification(i, k)
     )
 
 
 def in_vacation(i, j):
-    data = {
-        data["staff"][i]["name"]: data["staff"][i] for i in range(len(data["staff"]))
-    }
+    data = {data["staff"][i]["name"]: data["staff"][i] for i in range(len(data["staff"]))}
     data = data[i]["vacations"]
     return j in data
 
 
 def vacation_constraint(model, n_staff, horizon, n_qualifs, n_jobs, X):
-    model.addConstr(
+    model.addConstrs(
         X[i, j, k, l] == 0
-        for i in n_staff
-        for j in horizon
-        for k in n_qualifs
-        for l in n_jobs
+        for i in range(n_staff)
+        for j in range(horizon)
+        for k in range(n_qualifs)
+        for l in range(n_jobs)
         if in_vacation(i, j)
     )
 
